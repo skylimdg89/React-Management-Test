7e191cf515c6798631f5aeeef8eca09dd8ffad6d
DK <skylimdg89@gmail.com>
2021-12-12 22:13:21 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react'
 import './App.css';
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles} from '@material-ui/core/styles'
+
+// root 100% 그리고 위쪽으로 여백을 3의 가중치 만큼 가질수 있고, 전체 바깥쪽에 해당하는 X 쪽으로 overflow 발생할수 있도록 처리
+// 결과적으로 테이블은 1080 픽셀 이상 출력 될수 있도록 만든다
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -31,27 +51,27 @@ const customers = [
 
 class App extends Component {
   render(){
+    const {classes}=this.props
     return (
-      <div>
-         {
-           customers.map(c => {
-             return (
-               <Customer
-               key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-               />
-             )
-           })
-         }
-      </div>
-     
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
