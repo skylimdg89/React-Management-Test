e7a5885f3d405a6121737fa03d4e2731d255d7f2
DK <skylimdg89@gmail.com>
2021-12-12 20:22:28 +0900

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Geralt',
+  'birthday':'89',
+  'gender': 'male',
+  'job': 'Witcher'
+  },
+  {
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'Triss',
+  'birthday':'93',
+  'gender': 'female',
+  'job': 'Sorceress'
+  },
+  {
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Yen',
+  'birthday':'91',
+  'gender': 'female',
+  'job': 'sorceress'
+  }
+]
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+         {
+           customers.map(c => {
+             return (
+               <Customer
+               key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+               />
+             )
+           })
+         }
+      </div>
+     
+    );
+  }
 }
 
 export default App;
